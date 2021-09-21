a113362b86090a3eaa31182a37ebfba8edcb4ef4
CMEPTHNK321 <CMEPTHNK321@mail.ru>
2021-09-21 20:03:10 +0300

    Экранировал имя и текст, добавил красные сообщения о заполнении и хз че ще

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <meta charset="UTF-8">
         <title>Лист Комментариев</title>
         <style>
-            .eror {color: #ff0000;}
+            .error {color: #ff0000;}
         </style>
     </head>
     <body background="/images/fon.jpg">
@@ -21,7 +21,7 @@
         function test_input($data) {
             $data = trim($data);
             $data = stripslashes($data);
-            $data = htmlspecislchars($data);
+            $data = htmlspecialchars($data);
             return $data;
         }
 
@@ -31,74 +31,78 @@
             } else {
                 $name1 = test_input($_POST["name"]);
             }
+        }
 
-            //Проверяем чтоб были только Буквы и пробелы - без цифри всякой ерунды
-            if (!preg_match("/^[a-zA-Z ]*$/", $name1)) {
-                $name1Err = "Только буквы и пробелы допустимы в имени";
-            }
+        //Проверяем чтоб были только Буквы и пробелы - без цифри всякой ерунды
+        if (!preg_match("/^[a-zA-Z ]*$/", $name1)) {
+            $name1Err = "Только буквы и пробелы допустимы в имени";
+        }
+
+        If ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (empty($_POST["text"])) {
                 $text1Err = "Напишите Комментарий";
             } else {
-                $text1 = test_iput($_POST["text"]);
+                $text1 = test_input($_POST["text"]);
             }
-            ?>
+        }
+        ?>
 
         <!-- Оформляем кнопки для взаимодействия с PHP -->
-        
-            <p><font size=+2>Оставьте ваш комментарий</font></p>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                Имя:<input type="Text" name="name" size="20" placeholder="Ваше имя" maxlength="30" minlength="5" value="<?php echo $name1; ?>"> 
-                <span class="error">* <?php echo $name1Err; ?></span><br><br>
-                <textarea type="Text" name="text" rows="10" cols="70" placeholder="Ваш комментарий" maxlength="750" minlength="10" <?php echo $text1; ?>></textarea>
-                <span class="error">* <?php echo $text1Err; ?></span><br><br>
-                <input type="Submit" value="Отправить!">
-            </form>
-            <?php
-          
-            $name = $_POST["name"];
-            $text = $_POST["text"];
-            include "db_connect.php";
+
+        <p><font size=+2>Оставьте ваш комментарий</font></p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            Имя:<input type="Text" name="name" size="20" placeholder="Ваше имя" maxlength="30" minlength="5" value="<?php echo $name1; ?>"> 
+            <span class="error">* <?php echo $name1Err; ?></span><br><br>
+            <textarea type="Text" name="text" rows="10" cols="70" placeholder="Ваш комментарий" maxlength="750" minlength="10" <?php echo $text1; ?>></textarea>
+            <span class="error">* <?php echo $text1Err; ?></span><br><br>
+            <input type="Submit" value="Отправить!">
+        </form>
+
+        <?php
+        $name = $_POST["name"];
+        $text = htmlspecialchars($_POST["text"]);
+        include "db_connect.php";
 
 //пишем запрос
-            $str_sql_query1 = "INSERT INTO $tblName (name, text) VALUES ('$name', '$text')";
+        $str_sql_query1 = "INSERT INTO $tblName (name, text) VALUES ('$name', '$text')";
 
 //выполняем запрос от пользователя
-            if (!empty($name)) {
-                if (!empty($text)) {
-                    if (!mysqli_query($link, $str_sql_query1)) {
-                        echo "<br>He могу выполнить запрос на запись<br>";
-                    } else {
-                        echo "<br>Запись добавлена успешно<br>";
-                    }
+        if (!empty($name)) {
+            if (!empty($text)) {
+                if (!mysqli_query($link, $str_sql_query1)) {
+                    echo "<br>He могу выполнить запрос на запись<br>";
                 } else {
-                    echo "Заполните поле комментария";
+                    echo "<br>Запись добавлена успешно<br>";
                 }
+            } else {
+                echo "Заполните поле комментария";
             }
+        }
 
 
 // выполнение запроса на вывод всех комментариев
-            $str_sql_query4 = "SELECT * FROM $tblName";
+        $str_sql_query4 = "SELECT * FROM $tblName";
 
-            if (!$result = mysqli_query($link, $str_sql_query4)) {
-                echo "<br>He могу выполнить запрос<br>";
-                exit();
-            }
-            echo "<br>Запрос выполнен<br><br>";
+        if (!$result = mysqli_query($link, $str_sql_query4)) {
+            echo "<br>He могу выполнить запрос<br>";
+            exit();
+        }
+        echo "<br>Запрос выполнен<br><br>";
 // вывод результата запроса
-            while ($row = mysqli_fetch_assoc($result)) {
-                if ($row["deleted"] == 0) {
-                    print ($row["create_time"]);
-                    echo "&nbsp &nbsp <b>Пользователь:</b> &nbsp";
-                    print ($row["name"]);
-                    echo " <br> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <i><u>Комментарий</u></i><b>:</b> &nbsp";
-                    print ($row["text"]);
-                    echo "<br><br>";
-                }
+        while ($row = mysqli_fetch_assoc($result)) {
+            if ($row["deleted"] == 0) {
+                print ($row["create_time"]);
+                echo "&nbsp &nbsp <b>Пользователь:</b> &nbsp";
+                print ($row["name"]);
+                echo " <br> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <i><u>Комментарий</u></i><b>:</b> &nbsp";
+                print ($row["text"]);
+                echo "<br><br>";
             }
+        }
 // закрытие соединения с сервером базы данных
-            mysqli_close($link);
-            ?>
-            
+        mysqli_close($link);
+        ?>
+
     </body>
-    
+
 </html>
