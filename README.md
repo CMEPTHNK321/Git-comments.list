befbf0507a8539362339d100a8c4fbfdb0ab75a2
CMEPTHNK321 <CMEPTHNK321@mail.ru>
2021-09-21 18:54:47 +0300

    СОхраняем с ошибкой

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-#esfsfse
+  <?php
+        $sdb_name = "*****";
+        $user_name = "*****";
+        $user_password = "*****";
+        $db_name = "*****";
+        $tblName = "*****";
+// соединение с сервером базы данных
+        if (!$link = mysqli_connect($sdb_name, $user_name, $user_password)) {
+            echo "<br>He могу соединиться с сервером базы данных $sdb_name<br>";
+            exit();
+        }
+        echo "<br>Соединение с сервером базы данных $sdb_name установлено успешно<br>";
+// выбираем базу данных
+        if (!mysqli_select_db($link, $db_name)) {
+            echo "<br>He могу выбрать базу данных $db_name<br>";
+            exit();
+        }
+        echo "<br>Выбрана база данных $db_name<br>";
+// строка запроса
+        $str_sql_query = "CREATE TABLE $tblName (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(100) NOT NULL,
+  `text` text NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  UNIQUE KEY `\"$tblName\"_id_IDX` (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4"
+        ;
+// выполнение запроса
+        if (!mysqli_query($link, $str_sql_query)) {
+            echo "<br>He могу выполнить запрос на создание таблицы $tblName<br>";
+            exit();
+        }
+        echo "<br>Таблица $tblName создана успешно<br>";
+        // закрытие соединения с сервером базы данных
+        mysqli_close($link);
+        ?>
--- index.php
+++ index.php
@@ -1,58 +1,104 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Удаление</title>
+        <title>Лист Комментариев</title>
+        <style>
+            .eror {color: #ff0000;}
+        </style>
     </head>
     <body background="/images/fon.jpg">
-        <p><font size=+2>Оставьте ваш комментарий</font></p>
-        <form action="index.php" method="post">
-            Имя:<input type="Text" name="name" size="20" placeholder="Ваше имя" maxlength="12" minlength="5"><br>
-            <textarea type="Text" name="text" rows="10" cols="70" placeholder="Ваш комментарий" maxlength="750" minlength="10"></textarea><br>
-            <input type="Submit" value="Отправить!">
-        </form>
+
         <?php
-        $name = $_POST["name"];
-        $text = $_POST["text"];
-        include "db_connect.php";
+        // Определяем переменные и устанавливаем пустые значения
+        $name1Err = $text1Err = "";
+        $name1 = $text1 = "";
 
-        //пишем запрос
-        $str_sql_query1 = "INSERT INTO $tblName (name, text) VALUES ('$name', '$text')";
+        //Пишем функцию, которая будет проводить проверку :
+        //1.Функция хтмлспеЦиалчарс () преобразует специальные символы в сущности HTML.
+        //2.Прокладка ненужных символов (лишнего пространства, табуляции, новой строки) из входных данных пользователя (с помощью функции PHP Trim ())
+        //3.Удаление обратной косой черты (\) из входных данных пользователя (с помощью функции PHP stripslashes ())
 
-        //выполняем запрос от пользователя
-        if (!empty($name)) {
-            if (!empty($text)) {
-                if (!mysqli_query($link, $str_sql_query1)) {
-                    echo "<br>He могу выполнить запрос на запись<br>";
-                } else {
-                    echo "<br>Запись добавлена успешно<br>";
-                }
-            } else {
-                echo "Заполните поле комментария";
-            }
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecislchars($data);
+            return $data;
         }
+
+        If ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($_POST["name"])) {
+                $name1Err = "Введите имя";
+            } else {
+                $name1 = test_input($_POST["name"]);
+            }
+
+            //Проверяем чтоб были только Буквы и пробелы - без цифри всякой ерунды
+            if (!preg_match("/^[a-zA-Z ]*$/", $name1)) {
+                $name1Err = "Только буквы и пробелы допустимы в имени";
+            }
+            if (empty($_POST["text"])) {
+                $text1Err = "Напишите Комментарий";
+            } else {
+                $text1 = test_iput($_POST["text"]);
+            }
+            ?>
+
+        <!-- Оформляем кнопки для взаимодействия с PHP -->
+        
+            <p><font size=+2>Оставьте ваш комментарий</font></p>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                Имя:<input type="Text" name="name" size="20" placeholder="Ваше имя" maxlength="30" minlength="5" value="<?php echo $name1; ?>"> 
+                <span class="error">* <?php echo $name1Err; ?></span><br><br>
+                <textarea type="Text" name="text" rows="10" cols="70" placeholder="Ваш комментарий" maxlength="750" minlength="10" <?php echo $text1; ?>></textarea>
+                <span class="error">* <?php echo $text1Err; ?></span><br><br>
+                <input type="Submit" value="Отправить!">
+            </form>
+            <?php
+          
+            $name = $_POST["name"];
+            $text = $_POST["text"];
+            include "db_connect.php";
+
+//пишем запрос
+            $str_sql_query1 = "INSERT INTO $tblName (name, text) VALUES ('$name', '$text')";
+
+//выполняем запрос от пользователя
+            if (!empty($name)) {
+                if (!empty($text)) {
+                    if (!mysqli_query($link, $str_sql_query1)) {
+                        echo "<br>He могу выполнить запрос на запись<br>";
+                    } else {
+                        echo "<br>Запись добавлена успешно<br>";
+                    }
+                } else {
+                    echo "Заполните поле комментария";
+                }
+            }
 
 
 // выполнение запроса на вывод всех комментариев
-        $str_sql_query4 = "SELECT * FROM $tblName";
+            $str_sql_query4 = "SELECT * FROM $tblName";
 
-        if (!$result = mysqli_query($link, $str_sql_query4)) {
-            echo "<br>He могу выполнить запрос<br>";
-            exit();
-        }
-        echo "<br>Запрос выполнен<br><br>";
-// вывод результата запроса
-        while ($row = mysqli_fetch_assoc($result)) {
-            if ($row["deleted"] == 0) {
-                print ($row["create_time"]);
-                echo "&nbsp &nbsp <b>Пользователь:</b> &nbsp";
-                print ($row["name"]);
-                echo " <br> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <i><u>Комментарий</u></i><b>:</b> &nbsp";
-                print ($row["text"]);
-                echo "<br><br>";
+            if (!$result = mysqli_query($link, $str_sql_query4)) {
+                echo "<br>He могу выполнить запрос<br>";
+                exit();
             }
-        }
+            echo "<br>Запрос выполнен<br><br>";
+// вывод результата запроса
+            while ($row = mysqli_fetch_assoc($result)) {
+                if ($row["deleted"] == 0) {
+                    print ($row["create_time"]);
+                    echo "&nbsp &nbsp <b>Пользователь:</b> &nbsp";
+                    print ($row["name"]);
+                    echo " <br> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <i><u>Комментарий</u></i><b>:</b> &nbsp";
+                    print ($row["text"]);
+                    echo "<br><br>";
+                }
+            }
 // закрытие соединения с сервером базы данных
-        mysqli_close($link);
-        ?>
+            mysqli_close($link);
+            ?>
+            
     </body>
+    
 </html>
